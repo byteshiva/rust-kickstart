b4cd53312ae1cebe8bd27b962395b5c6b0396298
siva <byteshiva@gmail.com>
2020-02-14 10:59:14 +0530

    Modules and Visibility Example

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,21 @@
+// This declaration will look for a file named `my.rs` or `my/mod.rs` and will
+// insert its contents inside a module named `my` under this scope
+mod libs;
+
+fn function() {
+    println!("called `function()`");
+}
+
 fn main() {
+
+    libs::function();
+
+    function();
+
+    libs::indirect_access();
+
+    libs::nested::function();
+
     println!("Hello, world!");
     let s = "abc012è€";
     for i in 0..s.len() {
